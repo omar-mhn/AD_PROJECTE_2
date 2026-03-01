6be7d2fb08592d3b993be76ec2bfd3c57adb594e
omar <omar@>
2026-03-01 11:17:55 +0100

    implementación de schema.sql

---
+++ src/main/java/com/ra34/projecte2/Projecte2Application.java
@@ -0,0 +1,13 @@
+package com.ra34.projecte2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projecte2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projecte2Application.class, args);
+	}
+
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS Products(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    description VARCHAR(100),
+    stock INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    rating DECIMAL(3,2),
+    condition ENUM("nou","bon estat","acceptable", "mal estat"),
+    status BOOLEAN,
+    data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)
