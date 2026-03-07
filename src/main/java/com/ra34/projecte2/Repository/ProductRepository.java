aa93c5e8984c58f209ab6f4e83f7bb24d5e225af
omar <omar@>
2026-03-07 16:04:38 +0100

    search by prefix Done

--- src/main/java/com/ra34/projecte2/Repository/ProductRepository.java
+++ src/main/java/com/ra34/projecte2/Repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.ra34.projecte2.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ra34.projecte2.Model.Product;
 
 
 public interface ProductRepository extends JpaRepository<Product,Long>{
-    
+    List<Product>findByNameContainingAndStatusTrue(String name);
 }
