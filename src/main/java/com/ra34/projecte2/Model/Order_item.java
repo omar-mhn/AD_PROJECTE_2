15bc8efab2872b3e8b8a9e4b6bb72009a3c75b48
omar <omar@>
2026-04-22 19:22:36 +0200

    model de Order_item y Inovoice

--- src/main/java/com/ra34/projecte2/Model/Order_item.java
+++ src/main/java/com/ra34/projecte2/Model/Order_item.java
@@ -1,7 +1,20 @@
 package com.ra34.projecte2.Model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "order_item")
 public class Order_item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String description;
+
+    @Column(nullable = false)
+    private int quantity;
+    @Column(name = "unit_price" ,nullable = false)
+    private double unitPrice;
 }
