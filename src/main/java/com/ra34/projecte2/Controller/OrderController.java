e2c3bfcdb4156dbeeffe762dfabb4151832ec1f0
Mikleiper <mimuba55@gmail.com>
2026-04-26 19:33:54 +0200

    correccions

--- src/main/java/com/ra34/projecte2/Controller/OrderController.java
+++ src/main/java/com/ra34/projecte2/Controller/OrderController.java
@@ -4,19 +4,19 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ra34.projecte2.DTO.OrderDTO;
 import com.ra34.projecte2.DTO.OrderRequest;
 import com.ra34.projecte2.Service.OrderService;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class OrderController {
 
--- src/main/java/com/ra34/projecte2/Model/Condition.java
+++ src/main/java/com/ra34/projecte2/Model/Condition.java
@@ -1,5 +1,5 @@
 package com.ra34.projecte2.Model;
 
 public enum Condition {
-    nou,bon_estat,acceptable, mal_estat;
+    NOU,bon_estat,acceptable, mal_estat;
 }
